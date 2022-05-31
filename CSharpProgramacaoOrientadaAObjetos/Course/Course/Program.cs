55ccc467ed305a85fac5e88871d065a66515f5a8
wellington.xavier <wellington.xavier@financeiro24horas.com>
2022-05-31 17:55:44 -0300

    adicionado novos arquivos a pasta de CSharp.

---
+++ CSharpProgramacaoOrientadaAObjetos/Course/Course/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string produto1 = "Computador";
+            //string produto2 = "Mesa de escritório";
+            //double preco1 = 2100.00;
+            //double preco2 = 650.00;
+
+            //Console.WriteLine("Produtos:");
+            //Console.WriteLine(produto1 + ", cujo o preço é R$" + preco1);            
+            //Console.WriteLine(produto2 + ", cujo o preço é R$" + preco2);
+
+
+
+            //byte idade = 35;
+            //int codigo = 5290;
+            //char genero = 'M';
+
+            //Console.Write("Registro: ");
+            //Console.WriteLine(idade + " anos de idade, " + "código " + codigo + ", genero: " + genero);
+
+
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+
+            Console.WriteLine($"{preco1});
+
+
+        }
+    }
+}
