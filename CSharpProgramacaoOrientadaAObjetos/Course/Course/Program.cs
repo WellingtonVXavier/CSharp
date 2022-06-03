8b75d842dd29981939a952e4ca965baa7cf09d80
wellington.xavier <wellington.xavier@financeiro24horas.com>
2022-06-03 17:39:41 -0300

    adicionado novos exercicios

--- CSharpProgramacaoOrientadaAObjetos/Course/Course/Program.cs
+++ CSharpProgramacaoOrientadaAObjetos/Course/Course/Program.cs
@@ -7,34 +7,47 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            //string produto1 = "Computador";
-            //string produto2 = "Mesa de escritório";
-            //double preco1 = 2100.00;
-            //double preco2 = 650.00;
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            double preco1 = 2100.00;
+            double preco2 = 650.00;
 
-            //Console.WriteLine("Produtos:");
-            //Console.WriteLine(produto1 + ", cujo o preço é R$" + preco1);            
-            //Console.WriteLine(produto2 + ", cujo o preço é R$" + preco2);
+            Console.WriteLine("Produtos:");
+            Console.WriteLine(produto1 + ", cujo o preço é R$"+ preco1.ToString("F2"));
+            Console.WriteLine(produto2 + ", cujo o preço é R$" + preco2.ToString("F2"));
 
+            Console.WriteLine();
 
+            byte idade = 35;
+            int codigo = 5290;
+            char genero = 'M';
+            string nome = "Wellington, ";
 
-            //byte idade = 35;
-            //int codigo = 5290;
-            //char genero = 'M';
+            Console.Write("Registro: ");
+            Console.WriteLine(nome + idade + " anos de idade, " + "código " + codigo + ", genero: " + genero);
 
-            //Console.Write("Registro: ");
-            //Console.WriteLine(idade + " anos de idade, " + "código " + codigo + ", genero: " + genero);
+            Console.WriteLine();
 
-
-
-            double preco1 = 2100.0;
-            double preco2 = 650.50;
+            double preco3 = 2100.0;
+            double preco4 = 650.50;
             double medida = 53.234567;
 
 
-            Console.WriteLine($"{preco1});
+            Console.WriteLine("Medidas: {0:f8}", medida);
+            Console.WriteLine($"Medidas:  {medida:f3}");
+            Console.WriteLine("Medidas: " + medida.ToString("F3", CultureInfo.InvariantCulture));
 
+            Console.WriteLine();
 
+            Console.WriteLine("Medidas: {0:f8}", preco3);
+            Console.WriteLine($"Medidas:  {preco3:f3}");
+            Console.WriteLine("Medidas: " + preco3.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Medidas: {0:f8}", preco4);
+            Console.WriteLine($"Medidas:  {preco4:f3}");
+            Console.WriteLine("Medidas: " + preco4.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
---
+++ CSharpProgramacaoOrientadaAObjetos/Course3/Course3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Course3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+
+            string[] banana = Console.ReadLine().Split(' ');
+            string a = banana[0];
+            string b = banana[1];
+            string c = banana[2];
+
+
+            Console.WriteLine("Você digitou: ", frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
