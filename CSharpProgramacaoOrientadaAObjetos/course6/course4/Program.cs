0f82c26abe4e2cc1f7db72088b71c42b48e31d07
wellington.xavier <wellington.xavier@financeiro24horas.com>
2022-06-09 11:57:34 -0300

    Adicionei mais exercícios ao repositório.

---
+++ CSharpProgramacaoOrientadaAObjetos/course6/course4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace course4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual a hora atual?: ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("BOM DIA!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("BOA TARDE!");
+            }
+            else
+            {
+                Console.WriteLine("BOA NOITE!");
+            }
+
+        }
+    }
+}
